547d5b8d8726e8ddcf93a7444a7fedbc90c74f39
JEIDE2 <jeide2@unl.edu>
2020-04-02 13:43:32 -0500

    added Purchase class

---
+++ Lemon/Models/Purchase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lemon.Models
+{
+    public class Purchase
+    {
+        public int PurchaseId { get; set; }
+
+        public string PurchaseName { get; set; }
+
+        public string Description { get; set; }
+
+        public float Price { get; set; }
+
+        
+    }
+}
