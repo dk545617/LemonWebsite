487cd43e2c6b7e619c0cd93c377b81bbc9ab509b
Dylan Kirk <dylankirk16@outlook.com>
2020-04-09 13:46:13 -0500

    validation for question board

---
+++ Lemon/Data/Migrations/20200409184125_modifyQuestionClass.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Lemon.Data.Migrations
+{
+    public partial class modifyQuestionClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "Questions",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Post",
+                table: "Questions",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Questions",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "Questions",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Post",
+                table: "Questions",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Questions",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
